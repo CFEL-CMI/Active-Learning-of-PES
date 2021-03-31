7c279b72f52fef50e3e6a7a1eb0be22cc680825c
Jochen Küpper <jochen.kuepper@cfel.de>
2021-03-31 09:59:01 +0200

    Clean up a bit
    Should have been done by Yahya already.
    @Yayha, please add the code into alpes/ and scripts/cmi-alpes

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@
 from setuptools import setup
 
 copyright = 'Copyright (C) 2020 Jochen Küpper <jochen.kuepper@cfel.de>'
-name = "CMI Python-project template"
-version = "0.2.dev0"
+name = "Active Learning of PES"
+version = "1.0"
 release = version
-long_description = """CMI Python template
+long_description = """Active Learning of PES
 
-This is the installation and general build file of the CMI Python project template.
+This is the installation and general build file of the CMI Active Learning of PES code,
+which is part of and documented in ...
 
 Original author:    Jochen Küpper <jochen.kuepper@cfel.de>
 Current maintainer: Jochen Küpper <jochen.kuepper@cfel.de>
@@ -19,18 +20,18 @@ Current maintainer: Jochen Küpper <jochen.kuepper@cfel.de>
 
 
 setup(name=name,
-      python_requires     = '>=3.6',
+      python_requires     = '>=3.8',
       author              = "Jochen Küpper and the CFEL-CMI group",
       author_email        = "jochen.kuepper@cfel.de",
       maintainer          = "Jochen Küpper and the CFEL-CMI group",
       maintainer_email    = "jochen.kuepper@cfel.de",
       url                 = "https://github.com/CFEL-CMI/CMI-Python-project-template",
-      description         = "CMI Python-software template",
+      description         = "CMI Active Learning of PES",
       version             = version,
       long_description    = long_description,
       license             = "GPL",
-      packages            = ['cmitemplate'],
-      scripts             = ['scripts/cmitemplate_calc'],
+      packages            = ['alpes'],
+      scripts             = ['scripts/alpes'],
       command_options={
           'build_sphinx': {
               'project': ('setup.py', name),
