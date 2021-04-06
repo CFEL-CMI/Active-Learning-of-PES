47008f995bdfcbab6d5eaec22df4c0aedef43119
Yahya Saleh <yahya.saleh@desy.de>
2021-04-06 15:09:23 +0200

    changes to setupfile

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8; fill-column: 120 -*-
 #
-# Copyright (C) 2020 Jochen Küpper <jochen.kuepper@cfel.de>
+# Copyright (C) 2021 CFEL Controlled Molecule Imaging group
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-copyright = 'Copyright (C) 2020 Jochen Küpper <jochen.kuepper@cfel.de>'
+copyright = 'Copyright (C) 2021 Jochen Küpper <jochen.kuepper@cfel.de>'
 name = "Active Learning of PES"
 version = "1.0"
 release = version
@@ -14,25 +14,25 @@ long_description = """Active Learning of PES
 This is the installation and general build file of the CMI Active Learning of PES code,
 which is part of and documented in ...
 
-Original author:    Jochen Küpper <jochen.kuepper@cfel.de>
-Current maintainer: Jochen Küpper <jochen.kuepper@cfel.de>
+Author:             Yahya Saleh, Vishnu Sanjay, and the CFEL Controlled Molecule Imaging group <jochen.kuepper@cfel.de>
+Current maintainer: Yahya Saleh <yahya.saleh@cfel.de>
 """
-
 
 setup(name=name,
       python_requires     = '>=3.8',
-      author              = "Jochen Küpper and the CFEL-CMI group",
+      author              = "Yahya Saleh, Vishnu Sanjay, and the CFEL-CMI group",
       author_email        = "jochen.kuepper@cfel.de",
-      maintainer          = "Jochen Küpper and the CFEL-CMI group",
-      maintainer_email    = "jochen.kuepper@cfel.de",
+      maintainer          = "Yahya Saleh and the CFEL-CMI group",
+      maintainer_email    = "yahya.saleh@cfel.de",
       url                 = "https://github.com/CFEL-CMI/CMI-Python-project-template",
       description         = "CMI Active Learning of PES",
       version             = version,
       long_description    = long_description,
       license             = "GPL",
-      packages            = ['alpes'],
-      scripts             = ['scripts/alpes'],
-      command_options={
+      package_dir         = package_dir,
+      packages            = find_packages(where='scripts'),
+      #scripts             = ['scripts/alpes'],
+      command_options     = {
           'build_sphinx': {
               'project': ('setup.py', name),
               'version': ('setup.py', version),
@@ -40,4 +40,5 @@ setup(name=name,
               'source_dir': ('setup.py', 'doc'),
               'copyright': ('setup.py', copyright)}
       },
+      install_requires     = ['tensorflow>=2.2']
       )
