a47f5fe12bf766a2be1f4025370dea981b7f5a6d
Yahya Saleh <yahya.saleh@desy.de>
2021-04-23 09:15:14 +0200

    combined query strategies in a single file

--- scripts/query_strategies.py
+++ scripts/query_strategies.py
@@ -18,21 +18,65 @@
 # <http://www.gnu.org/licenses/>.
 
 """
-An implementation of stochastic query by committee (QBC) algorithm for regression problems in a batch setting. The members of the
-committee are regression trees. The disagreement criterion between the trees is the standard deviation from their mean prediction.
-Data to query are chosen by a random samplin procedure that gives higher weights for data points corresponding to uncertainties in their
-predictions.
+An implementation of query by committee (QBC), stochastic query by forest (SQBF), and random sampling (RS) algorithms
+for regression problems in a batch setting. The members of the committee in QBC and SQBF are regression trees.
+The disagreement criterion between the trees is the standard deviation of the predictions of the trees.
 """
 
 import numpy as np
 from libact.base.dataset import Dataset
 from libact.base.interfaces import QueryStrategy
 
+class QBCR(QueryStrategy):
+    """QBC query strategy object.
+    Args:
+    BatchSize: int. The number of elements to query in a single AL iteration.
+
+    Trees: an ensemble of trees sklearn instance composed of n trees.
+
+    dataset: an object containing the labeled and unlabeled data.
+
+    Attribs:
+    make_query: choose n=BatchSize data points to query from the pool.
+    """
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        self.BatchSize = kwargs.pop('BatchSize', 1)
+        self.Trees = kwargs.pop('Trees', None)
+        if self.Trees is None:
+            raise TypeError(
+                "__init__() missing required keyword-only argument: 'Trees'"
+            )
+
+    def _standard_deviation(self, predictions):
+        return np.std(predictions, axis=0)
+
+    def make_query(self):
+        # get the unlabeled entries, i.e. the pool data
+        unlabeled_entry_ids, X_pool = self.dataset.get_unlabeled_entries()
+        if len(unlabeled_entry_ids)>self.BatchSize:
+            # Predict the energies of the unlabeled data
+            predictions = np.array([estimator.predict(X_pool) for estimator in self.Trees.estimators_])
+            # compute the standard deviation of predictions for every unlabeled datapoint
+            stddev = self._standard_deviation(predictions)
+            ask_idx = np.flip(np.argsort(stddev))[0:self.BatchSize]
+        else:
+            #The number of remaining unlabeled data is less than batch size, query the remaining data
+            ask_idx = np.arange(len(unlabeled_entry_ids))
+
+        return unlabeled_entry_ids[ask_idx]
+
+    def _name(self):
+        return "QBC"
+
 class SQBF(QueryStrategy):
     """SQBF query strategy object
     Args:
     BatchSize: int. The number of elements to query in a single AL iteration.
+
     Trees: an ensemble of trees sklearn instance composed of n trees.
+
     dataset: an object containing the labeled and unlabeled data.
 
     Attribs:
@@ -86,3 +130,33 @@ class SQBF(QueryStrategy):
 
     def _name(self):
         return "SQBF"
+
+class RS(QueryStrategy):
+    """RS query strategy object
+
+    Args:
+    BatchSize: int. The number of elements to query in a single AL iteration.
+
+    dataset: an object containing the labeled and unlabeled data.
+
+    Attribs:
+    make_query: chooses n=BatchSize data points to query from the pool.
+    """
+
+    def __init__(self, dataset, **kwargs):
+
+        super(RandomSampling, self).__init__(dataset, **kwargs)
+        self.BatchSize = kwargs.pop('BatchSize', 1)
+
+    def make_query(self):
+        # get the unlabeled entries, i.e. the pool data
+        unlabeled_entry_ids, X_pool = self.dataset.get_unlabeled_entries()
+        if len(unlabeled_entry_ids)>self.BatchSize:
+            ask_idx = np.random.choice(np.shape(X_pool)[0], size=self.BatchSize, replace=False)
+        else:
+            #The number of remaining unlabeled data is less than batch size, query the remaining data
+            ask_idx = np.arange(len(unlabeled_entry_ids))
+        return unlabeled_entry_ids[ask_idx]
+
+    def _name(self):
+        return "RS"
